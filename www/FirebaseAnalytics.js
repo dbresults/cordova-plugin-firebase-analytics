ae4eafc352d2b3fa0040cbd68737da6ec3f83e8b
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2023-01-11 18:58:17 +0000

    test: use module.exports

--- www/FirebaseAnalytics.js
+++ www/FirebaseAnalytics.js
@@ -1,7 +1,6 @@
 var exec = require("cordova/exec");
 var PLUGIN_NAME = "FirebaseAnalytics";
 
-/*
 module.exports = {
     logEvent: function(name, params) {
         return new Promise(function(resolve, reject) {
@@ -50,29 +49,13 @@ module.exports = {
             exec(resolve, reject, PLUGIN_NAME, "setDefaultEventParameters", [defaults || {}]);
         });
     },
-    requestTrackingAuthorization: function(showInformation, title, message, buttonTitle) {
-        return new Promise(function(resolve, reject) {
-
-            if(showInformation) {
-                if (typeof title !== "string") {
-                    return reject(new TypeError("Title property name must be a string"));
-                }
-    
-                if (typeof message !== "string") {
-                    return reject(new TypeError("Message property value must be a string"));
-                }
-    
-                if (typeof buttonTitle !== "string") {
-                    return reject(new TypeError("Button title property value must be a string"));
-                }
-            }
-
-            exec(resolve, reject, PLUGIN_NAME, "requestTrackingAuthorization", [showInformation, title, message, buttonTitle]);
-        });
+    requestTrackingAuthorization: function(showInformation, title, message, buttonTitle, success, error) {
+        exec(success, error, 'FirebaseAnalytics', 'requestTrackingAuthorization', [showInformation, title, message, buttonTitle]);
     }
 };
-*/
 
+/*
 exports.requestTrackingAuthorization = function(showInformation, title, message, buttonTitle, success, error) {
     exec(success, error, 'FirebaseAnalytics', 'requestTrackingAuthorization', [showInformation, title, message, buttonTitle]);
 }
+*/
